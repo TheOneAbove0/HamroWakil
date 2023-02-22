454bd6335807c81e09d9bd8e89575a838c2b43e3
anuj-tech <64081087+anuj-tech@users.noreply.github.com>
2023-02-22 12:26:12 +0545

    Update styles.js

--- lawyer/src/admin/styles.js
+++ lawyer/src/admin/styles.js
@@ -2,7 +2,47 @@ import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme();
 
+export const container = {
+  mt: "12vh",
+  ml: "5vw",
+  minHeight: "100vh",
+  background: "linear-gradient(135deg, #dff6fe 30%, #0095de 90%)",
+  backgroundImage: `url('../images/blue2.jpg')`,
+  backgroundPosition: "center",
+  backgroundRepeat: "repeat-x",
+  backgroundSize: "cover",
+  [theme.breakpoints.down("md")]: {
+    mt: "10vh",
+    ml: "10vw",
+    maxWidth: "95vw",
+  },
+  [theme.breakpoints.down("sm")]: {
+    ml: "12vw",
+    maxWidth: "80vw",
+  },
+};
 
+export const paper = {
+  p: 2,
+  display: "flex",
+  flexDirection: "column",
+  //backgroundColor: "transparent",
+};
+
+export const listItem = {
+  border: "2px solid #0d7da5",
+  borderRadius: "25px",
+  margin: "2px",
+  boxShadow: 2,
+  backgroundColor: "#e6f7fd",
+  "&:hover": {
+    backgroundColor: "#d0f0fb",
+    boxShadow: 3,
+    border: "3px solid #0d7da5",
+  },
+};
+
+export const typography = { fontWeight: "bold", color: "#063547" };
 
 export const signinGrid = {
   backgroundImage: `url('images/admin_signin.jpeg')`,
